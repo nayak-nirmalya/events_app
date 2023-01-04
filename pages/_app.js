18fc2db7bdd539c4ceb8364c16869ffb309f171a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-04 19:28:53 +0530

    added sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import MainLayout from '../src/components/layout/main-layout'
 import '../styles/globals.css'
+import '../styles/Home.sass'
 
 export default function App({ Component, pageProps }) {
   return (
