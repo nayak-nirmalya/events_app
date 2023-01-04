e646ca7ece5f99ae21d1030af692f6da7758eb00
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-04 20:49:05 +0530

    added styling to home-page footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,28 +3,31 @@ import Link from 'next/link'
 
 export const Header = () => (
   <header>
-    <div className="topNav">
-      <Image
-        alt="Nirmalya Nayak"
-        src={'/images/logo_black.png'}
-        width={50}
-        height={50}
-      />
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/events">Events</Link>
-          </li>
-          <li>
-            <Link href="/about-us">About Us</Link>
-          </li>
-        </ul>
-        <img />
-      </nav>
+    <div>
+      <div className="topNav">
+        <Image
+          className="img"
+          alt="Nirmalya Nayak"
+          src={'/images/logo_black.png'}
+          width={50}
+          height={50}
+        />
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+            <li>
+              <Link href="/about-us">About Us</Link>
+            </li>
+          </ul>
+          <img />
+        </nav>
+      </div>
+      <h1>Header H1 Tag. Hi, Everyone.</h1>
     </div>
-    <h1>Header H1 Tag. Hi, Everyone.</h1>
   </header>
 )
--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export const HomePage = ({ data }) => (
-  <>
-    {data.map((event, index) => (
-      <div key={index} id={event.id}>
-        <Link href={`/events/${event.id}`}>
+  <div className="home_body">
+    {data?.map((event, index) => (
+      <Link key={index} className="card" href={`/events/${event.id}`}>
+        <div className="image">
           <Image
             priority
             width={200}
@@ -13,10 +13,12 @@ export const HomePage = ({ data }) => (
             alt={event.title}
             src={event.image}
           />
+        </div>
+        <div className="content">
           <h2>{event.title}</h2>
           <p>{event.description}</p>
-        </Link>
-      </div>
+        </div>
+      </Link>
     ))}
-  </>
+  </div>
 )
