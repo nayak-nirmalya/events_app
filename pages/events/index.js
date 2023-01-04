c574fed2abebc8cebbd18607d372c673d8f44204
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-04 13:08:08 +0530

    showing events dynamically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,34 @@
-const EventsPage = () => {
+import Image from 'next/image'
+
+const EventsPage = ({ data }) => {
   return (
     <div>
       <h1>Event</h1>
-      <div>
-        <h2>Events in India</h2>
-        <h2>Events in Kendrapara</h2>
-        <h2>Events in Odisha</h2>
-      </div>
+      {data.map((event, index) => (
+        <a key={index} href={`/events/${event.id}`}>
+          <Image
+            priority={true}
+            src={event.image}
+            width={200}
+            height={200}
+            alt={event.title}
+          />
+          <h2>{event.title}</h2>
+        </a>
+      ))}
     </div>
   )
 }
 
 export default EventsPage
+
+export async function getStaticProps() {
+  const { events_categories } = await import('../../data/data.json')
+  // console.log(allEvents)
+
+  return {
+    props: {
+      data: events_categories,
+    },
+  }
+}
