7b64e717d2ae4abb332922954e824339f0c94022
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-04 19:34:56 +0530

    added logo.png

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,22 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export const Header = () => (
   <header>
-    <nav>
-      <img />
-      <Link href="/">Home</Link>
-      <Link href="/events">Events</Link>
-      <Link href="/about-us">About Us</Link>
-    </nav>
+    <div>
+      <Image
+        alt="Nirmalya Nayak"
+        src={'/images/logo_black.png'}
+        width={50}
+        height={50}
+      />
+      <nav>
+        <img />
+        <Link href="/">Home</Link>
+        <Link href="/events">Events</Link>
+        <Link href="/about-us">About Us</Link>
+      </nav>
+    </div>
+    <h1>Header H1 Tag. Hi, Everyone.</h1>
   </header>
 )
