32d0fd02bb17097cc58a5dd9a70ddfc7aa9f87cc
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-04 15:44:50 +0530

    updated singlel event details page

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,5 +1,46 @@
-const EventPage = () => {
-  return <h1>Single Event Page</h1>
+import Image from 'next/image'
+
+const EventPage = ({ data }) => {
+  return (
+    <div>
+      <Image
+        priority
+        src={data.image}
+        width={1000}
+        height={500}
+        alt={data.title}
+      />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  )
 }
 
 export default EventPage
+
+export async function getStaticPaths() {
+  const { allEvents } = await import('/data/data.json')
+  const allPaths = allEvents.map((event) => {
+    return {
+      params: {
+        cat: event.city,
+        id: event.id,
+      },
+    }
+  })
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id
+  const { allEvents } = await import('/data/data.json')
+  const eventData = allEvents.find((event) => id === event.id)
+
+  return {
+    props: { data: eventData },
+  }
+}
