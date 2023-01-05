0e993e4441c28d114a2e1a2d7044dc19b6445a30
Nirmalya Nayak <n.nayak263@gmail.com>
2023-01-05 10:52:14 +0530

    added styling to events page

--- src/components/events/events-page.jsx
+++ src/components/events/events-page.jsx
@@ -3,14 +3,13 @@ import Link from 'next/link'
 
 const EventsPageJSX = ({ data }) => (
   <div className="events_page">
-    <h1>Events Page</h1>
     {data.map((event, index) => (
-      <Link key={index} href={`/events/${event.id}`}>
+      <Link className="card" key={index} href={`/events/${event.id}`}>
         <Image
           priority={true}
           src={event.image}
-          width={200}
-          height={200}
+          width={300}
+          height={300}
           alt={event.title}
         />
         <h2>{event.title}</h2>
